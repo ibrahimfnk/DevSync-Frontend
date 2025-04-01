0150e10e648fab316132197a1ae0628536263a89
ibrahimfnk <ibrahimkadiri.uae@gmail.com>
2025-04-02 00:21:28 +0530

    removed lanyard

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Lanyard from '../ui/lanyard';
+
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-           <Lanyard position={[0, 0, 30]} gravity={[0, -40, 0]} fov={20} transparent={true} />  
+            <h1>About</h1>  
         </div>
     );
 };
